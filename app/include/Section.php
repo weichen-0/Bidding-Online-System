2ff947e5ebc42b438781d1ed02fdb3351ea6b8a3
BEH Wei Chen <wcbeh.2018@sis.smu.edu.sg>
2019-09-17 12:39:31 +0800

    Created section include files

---
+++ app/include/Section.php
@@ -0,0 +1,59 @@
+<?php
+
+class Section {
+    // property declaration
+    public $course;
+    public $section;
+    public $day;    
+    public $start;
+    public $end;
+    public $instructor;
+    public $venue;
+    public $size;
+    
+    public function __construct($course, $section, $day, $start, $end, $instructor, $venue, $size) {
+        $this->course = $course;
+        $this->section = $section;
+        $this->day = $day;
+        $this->start = $start;
+        $this->end = $end;
+        $this->instructor = $instructor;
+        $this->venue = $venue;
+        $this->size = $size;
+    }
+
+    // checks if the class timetable for this section clashes with that of another section
+    public function classClashWith($section) {
+        
+        // return false immediately if on different days
+        if ($this->day != $section->day) {
+            return false;
+        }
+
+        $dateToday = date('Ymd');
+
+        // Since we only want to compare the time, it doesn't matter as long as we use same date throughout for consistent datetime
+        $current_start = convertDateTime($dateToday, $this->start);
+        $current_end = convertDateTime($dateToday, $this->end);
+
+        $other_start = convertDateTime($dateToday, $course->start);
+        $other_end = convertDateTime($dateToday, $course->end);
+        
+        // if the class for this section starts or ends during that of another section
+        if (($current_start >= $other_start && $current_start <= $other_end) || ($current_end >= $other_start && $current_end <= $other_end)) {
+            return true;
+        }
+
+        return false;
+    }
+
+    // converts date and time to iso_datetime for ease of comparison
+    private function convertDateTime($date, $time) {
+        $datetime = $date . ' ' . $time;
+        $dateObj = DateTime::createFromFormat("Ymd H:i", $datetime);
+        return $dateObj->format(Datetime::ATOM);
+    }
+
+}
+
+?>
