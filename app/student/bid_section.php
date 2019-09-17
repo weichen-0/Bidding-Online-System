e189e7c689d13e5532d5d53e6a3ddad6e04ef893
BEH Wei Chen <wcbeh.2018@sis.smu.edu.sg>
2019-09-17 15:37:15 +0800

    Update

---
+++ app/student/bid_section.php
@@ -0,0 +1,59 @@
+<?php
+    require_once '../include/common.php';
+    require_once '../include/protect.php';
+
+    $student_dao = new StudentDAO();
+    $student = $student_dao->retrieve($_SESSION['userid']);
+?>
+
+<html>
+    <head>
+        <link rel="stylesheet" type="text/css" href="../include/style.css">
+    </head>
+    <body>
+        <h1>Bid Section</h1>
+        <p>
+            <a href='index.php'>Home</a> |
+            <a href='drop_bid.php'>Drop Bid</a> |
+            <a href='drop_section.php'>Drop Section</a> |   
+            <a href='../logout.php'>Logout</a>
+        </p>
+        <p>
+            Account Balance: <big><b><u>e$<?=$student->edollar?></u></b></big>
+        </p>
+        <form method='POST' action='bid_section_process.php'>
+        <table>
+            <tr>
+                <td>Course</td>
+                <td>
+                    <input name='course'/>
+                </td>
+            </tr>
+            <tr>
+                <td>Section</td>
+                <td>
+                    <input name='section'/>
+                </td>
+            </tr>
+            <tr>
+                <td>Bid (e$)</td>
+                <td>
+                    <input name='amount'/>
+                </td>
+            </tr>
+            <tr>
+                <td colspan='2'>
+                    <input name='submit' type='submit' />
+                </td>
+            </tr>
+        </form>
+
+<?php
+
+?>
+        
+        </table>
+
+    </body>
+
+</html>
--- app/student/index.php
+++ app/student/index.php
@@ -4,6 +4,9 @@
 
     $dao = new StudentDAO();
     $student = $dao->retrieve($_SESSION['userid']);
+
+    $bid_dao = new BidDAO();
+    $bids = $bid_dao->retrieve($student->userid);
 ?>
 
 <html>
@@ -13,8 +16,35 @@
     <body>
         <h1>Welcome <?=$student->name?>!</h1>
         <p>
+            <a href='bid_section.php'>Bid Section</a> |
+            <a href='drop_bid.php'>Drop Bid</a> |
+            <a href='drop_section.php'>Drop Section</a> |   
             <a href='../logout.php'>Logout</a>
         </p>
+        <p>
+            Account Balance: <big><b><u>e$<?=$student->edollar?></u></b></big>
+        </p>
+        <table>
+            <tr>
+                <th>Course ID</th>
+                <th>Section</th>
+                <th>Bid Amount</th>
+                <th>Status</th>
+            </tr>
+
+<?php
+        foreach ($bids as $bid) {
+            echo "<tr>
+                    <td>{$bid->code}</td>
+                    <td>{$bid->section}</td>
+                    <td>{$bid->amount}</td>
+                    <td>...</td>
+                </tr>";
+        }
+?>
+        
+        </table>
+
     </body>
 
 </html>
