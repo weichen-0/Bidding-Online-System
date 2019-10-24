d0aaf28ec5f2c744f011946d978b714d52c8b9e3
BEH Wei Chen <wcbeh.2018@sis.smu.edu.sg>
2019-10-24 21:14:25 +0800

    Update bid_section.php

--- app/student/bid_section.php
+++ app/student/bid_section.php
@@ -95,59 +95,65 @@ function validateSection($section) {
             Bidding Round <?=$round_dao->retrieveRound()?>: <big><b><u><?=$round_dao->retrieveStatus()?></u></b></big>
         </p>
     
-        <div style="overflow-y:auto; max-height:300px; background-color:darkgrey; display:inline-block;">
-        <p style='margin-top:5px; margin-bottom:5px; text-align:center; text'><b>Available Courses</b></p>
-        <table>
-                <tr>
-                    <th>Course ID</td>
-                    <th>Section</td>
-                    <th>Day</td>
-                    <th>Start</td>
-                    <th>End</td>
-                    <th>Instructor</td>
-                    <th>Venue</td>
-                    <th>Size</td>
-                    <th>Vacancy</td>
-                </tr>
+        <div style="background-color:darkgrey; display:inline-block;">
+            <p style='margin-top:5px; margin-bottom:5px; text-align:center; text'><b>Scroll table to see all available courses</b></p>
+            <table>
+                    <tr>
+                        <th width='74'>Course ID</td>
+                        <th width='50'>Section</td>
+                        <th width='30'>Day</td>
+                        <th width='36'>Start</td>
+                        <th width='36'>End</td>
+                        <th width='115'>Instructor</td>
+                        <th width='114'>Venue</td>
+                        <th width='28'>Size</td>
+                        <th width='57'>Vacancy</td>
+                    </tr>
+            </table>
+
+            <div style='overflow-y:auto; max-height:450px;'>
+                <table>
 <?php
-        $section_dict = array();
-        $days = ["Mon", "Tue", "Wed", "Thu", "Fri"];
+                $section_dict = array();
+                $days = ["Mon", "Tue", "Wed", "Thu", "Fri"];
 
-        foreach ($section_dao->retrieveAll() as $section) {
-            if (isset($section_dict[$section->course])) {
-                $section_dict[$section->course][] = $section;
-            } else {
-                $section_dict[$section->course] = [$section];
-            }
-        }
-
-        foreach ($section_dict as $key => $list) {
-            $num_of_sections = count($list);
-            echo "<tr>
-                    <td rowspan='$num_of_sections'>$key</td>";
-            foreach ($list as $section) {   
-                $section_enrolments = $enrolment_dao->retrieveBySection($section->course, $section->section);
-                $vacancy = $section->size - count($section_enrolments);
-                $isValid = validateSection($section);
-
-                $error_style = "";
-                if (!$isValid) {
-                    $error_style = "style='background-color:firebrick'";
+                foreach ($section_dao->retrieveAll() as $section) {
+                    if (isset($section_dict[$section->course])) {
+                        $section_dict[$section->course][] = $section;
+                    } else {
+                        $section_dict[$section->course] = [$section];
+                    }
                 }
 
-                echo "<td $error_style>{$section->section}</td>
-                <td>{$days[$section->day - 1]}</td>
-                <td>{$section->start}</td>
-                <td>{$section->end}</td>
-                <td>{$section->instructor}</td>
-                <td>{$section->venue}</td>
-                <td>{$section->size}</td>
-                <td>{$vacancy}</td></tr>";
-            }                    
-        }
+                foreach ($section_dict as $key => $list) {
+                    $num_of_sections = count($list);
+                    echo "<tr>
+                            <td width='74'rowspan='$num_of_sections'>$key</td>";
+                    foreach ($list as $section) {   
+                        $section_enrolments = $enrolment_dao->retrieveBySection($section->course, $section->section);
+                        $vacancy = $section->size - count($section_enrolments);
+                        $isValid = validateSection($section);
+
+                        $error_style = "";
+                        if (!$isValid) {
+                            $error_style = "style='background-color:firebrick'";
+                        }
+
+                        echo "<td width='50' $error_style>{$section->section}</td>
+                        <td width='30'>{$days[$section->day - 1]}</td>
+                        <td width='36'>{$section->start}</td>
+                        <td width='36'>{$section->end}</td>
+                        <td width='115'>{$section->instructor}</td>
+                        <td width='114'>{$section->venue}</td>
+                        <td width='28'>{$section->size}</td>
+                        <td width='57'>{$vacancy}</td></tr>";
+                    }                    
+                }
 ?>
-        </table>        
+                </table>        
+            </div>
         </div>
+
         <br/><br/>
         
         <form method='POST' action='bid_section_process.php'>
